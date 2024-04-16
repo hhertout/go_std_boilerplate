4d2b35ca72f22d8ffff5230933effe6ccf9f3251
hhertout <hugues.hertout@gmail.com>
2024-04-16 22:34:16 +0200

    chore: adding log at server start

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
 	"std_go_boilerplate/internal/server"
 )
 
 func main() {
 	server := server.NewServer()
 
+	log.Printf("🚀 Server running on port %s\n", server.Addr)
 	err := server.ListenAndServe()
 	if err != nil {
 		panic("cannot start server")
